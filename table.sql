91537ff38df57993f80ba5360d08be81d037e5c8
newvid <thtjwls@newvid.co.kr>
2017-04-26 19:03:20 +0900

    테이블수정

--- table.sql
+++ table.sql
@@ -9,14 +9,14 @@
  * view : 글번호,댓글, 카테고리, 글제목, 작성자, 뷰, 좋아요
  * option : 삭제여부, 비밀글여부
  */
-CREATE TABLE newstory_category(
+CREATE TABLE nb_category(
   idx VARCHAR(6) NOT NULL PRIMARY KEY COMMENT '카테고리 id',
   name VARCHAR(20) NULL UNIQUE KEY COMMENT '카테고리 이름',
   sub_cnt INT NOT NULL COMMENT '서브카테고리 갯수',
   path VARCHAR(30) NOT NULL DEFAULT '/' COMMENT '카테고리 경로'
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8 COMMENT = '카테고리 테이블';
 
-CREATE TABLE newstory_list (
+CREATE TABLE nb_list (
   idx INT not null PRIMARY KEY AUTO_INCREMENT comment '글번호 (code)',
   FK_category VARCHAR(6) NOT NULL comment '글 카테고리',
   subject VARCHAR(50) NOT NULL comment '글 제목',
@@ -28,62 +28,70 @@ CREATE TABLE newstory_list (
   in_use TINYINT NOT NULL DEFAULT '1' comment '사용여부 (0 삭제, 1 사용)',
   in_secret TINYINT NOT NULL DEFAULT '1' comment '비밀글 여부 (0 삭제, 1 사용)',
   FOREIGN KEY (`writer`) REFERENCES `users` (`idx`),
-  FOREIGN KEY (`FK_category`) REFERENCES `newstory_category` (`idx`)
+  FOREIGN KEY (`FK_category`) REFERENCES `nb_category` (`idx`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
-CREATE TABLE newstory_comment(
+CREATE TABLE nb_comment(
   idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT comment '댓글번호 (code)',
-  pid INT NOT NULL comment '글번호 (code, foreign key)',
+  pid INT NOT NULL comment '원글번호 (code, foreign key)',
   contents TEXT NOT NULL comment '댓글 내용',
   regist DATETIME NOT NULL comment '댓글 작성일',
   writer INT NOT NULL comment '댓글 작성자 (code,foreign key)',
   FOREIGN KEY (`writer`) REFERENCES `users` (`idx`),
-  FOREIGN KEY (`pid`) REFERENCES `newstory_list` (`idx`)
+  FOREIGN KEY (`pid`) REFERENCES `nb_list` (`idx`)
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8 COMMENT = '블로그 뷰';
 
-CREATE TABLE newstory_files (
+CREATE TABLE nb_comment_step(
+  idx int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '인덱스',
+  pid INT NOT NULL COMMENT '원댓글 번호',
+  contents TEXT NOT NULL COMMENT '답글 컨텐츠',
+  regist DATETIME NOT NULL COMMENT '작성일',
+  writer INT NOT NULL COMMENT '답글 작성자',
+  FOREIGN KEY (`writer`) REFERENCES `users` (`idx`),
+  FOREIGN KEY (`idx`) REFERENCES `nb_comment` (`idx`)
+) ENGINE = InnoDB DEFAULT CHAR SET = UTF8 COMMENT ='댓글 > 답글';
+
+CREATE TABLE nb_files (
   pid INT NOT NULL comment '글번호 (code, foreign key)',
   file_name VARCHAR(50) NOT NULL,
   file_type VARCHAR(10) NOT NULL,
   file_regist DATETIME NOT NULL,
-  FOREIGN KEY (`pid`) REFERENCES `newstory_list` (`idx`)
+  FOREIGN KEY (`pid`) REFERENCES `nb_list` (`idx`)
 )ENGINE = InnoDB DEFAULT CHARSET = UTF8 COMMENT = '블로그 파일';
 
-
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('000000','HOME',0,'/');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('100000','취미·유머',0,'/hobby');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('200000','문화',0,'/culture');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('300000','강의',0,'/lecture');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('400000','IT',0,'/it');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('500000','Design',0,'/design');
+INSERT INTO nb_category (idx, name, sub_cnt, path)
+VALUES ('600000','여행·맛집',0,'/travel');
 /**
 insert test data
  */
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('000000','HOME',0,'/');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('100000','취미·유머',0,'/hobby');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('200000','문화',0,'/culture');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('300000','강의',0,'/lecture');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('400000','IT',0,'/it');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('500000','Design',0,'/design');
-INSERT INTO newstory_category (idx, name, sub_cnt, path)
-VALUES ('600000','여행·맛집',0,'/travel');
+
+INSERT INTO nb_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
+VALUES ('200000','1번째 테스트 제목','첫번째 테스트 본문내용',2,3333,503,'now()',1,0);
+INSERT INTO nb_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
+VALUES ('200000','2번째 테스트 제목','2번째 테스트 본문내용',2,3333,503,'now()',1,0);
+INSERT INTO nb_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
+VALUES ('200000','3번째 테스트 제목','3번째 테스트 본문내용',2,3333,503,'now()',1,0);
+INSERT INTO nb_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
+VALUES ('200000','4번째 테스트 제목','4번째 테스트 본문내용',2,3333,503,'now()',1,0);
 
 
-INSERT INTO newstory_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
-VALUES ('200000','1번째 테스트 제목','첫번째 테스트 본문내용',1,3333,503,'now()',1,0);
-INSERT INTO newstory_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
-VALUES ('200000','2번째 테스트 제목','2번째 테스트 본문내용',1,3333,503,'now()',1,0);
-INSERT INTO newstory_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
-VALUES ('200000','3번째 테스트 제목','3번째 테스트 본문내용',1,3333,503,'now()',1,0);
-INSERT INTO newstory_list (FK_category,subject,contents,writer,views,likes,regist,in_use,in_secret)
-VALUES ('200000','4번째 테스트 제목','4번째 테스트 본문내용',1,3333,503,'now()',1,0);
-
-
-INSERT INTO newstory_comment (pid,contents,regist,writer)
-VALUES (1,'1번째 글의 1번째 댓글','now()',1);
-INSERT INTO newstory_comment (pid,contents,regist,writer)
-VALUES (2,'2번째 글의 1번째 댓글','now()',1);
-INSERT INTO newstory_comment (pid,contents,regist,writer)
-VALUES (1,'1번째 글의 2번째 댓글','now()',1);
-INSERT INTO newstory_comment (pid,contents,regist,writer)
-VALUES (1,'3번째 글의 3번째 댓글','now()',1);
+INSERT INTO nb_comment (pid,contents,regist,writer)
+VALUES (1,'1번째 글의 1번째 댓글','now()',2);
+INSERT INTO nb_comment (pid,contents,regist,writer)
+VALUES (2,'2번째 글의 1번째 댓글','now()',2);
+INSERT INTO nb_comment (pid,contents,regist,writer)
+VALUES (1,'1번째 글의 2번째 댓글','now()',2);
+INSERT INTO nb_comment (pid,contents,regist,writer)
+VALUES (1,'3번째 글의 3번째 댓글','now()',2);
--- test.php
+++ test.php
@@ -9,7 +9,82 @@
 </head>
 <body>
 <?php
-echo $_SERVER['CI_ENV'];
+$mysqli = new mysqli('localhost','root','autoset','test');
+if ( mysqli_connect_error() ) {
+    exit('Connect Error ('. mysqli_connect_errno(). ') '.mysqli_connect_error());
+}
+
+
+
+/**
+ * @return string
+ * 하단에는 베이스 쿼리문
+ */
+
+function inserted() {
+    $date = date('Y-m-d H:i:s');
+
+    $query = "
+        INSERT INTO testtable (Id,Regist_day) VALUES ('thtjwls','{$date}');
+        INSERT INTO testtable (Id,Regist_day) VALUES ('newvid','{$date}');
+        INSERT INTO testtable (Id,Regist_day) VALUES ('dayekim','{$date}');
+        INSERT INTO testtable (Id,Regist_day) VALUES ('jihoon','{$date}');
+        INSERT INTO testtable (Id,Regist_day) VALUES ('dev','{$date}');
+    ";
+
+    return $query;
+}
+
+function inserted2() {
+    $date = date('Y-m-d H:i:s');
+    
+    $query = "
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (1,'첫번째 아이디의 글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (1,'첫번째 아이디의 2번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (1,'첫번째 아이디의 3번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (1,'첫번째 아이디의 4번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (2,'2번째 아이디의 1번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (2,'2번째 아이디의 2번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (2,'2번째 아이디의 3번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (3,'3번째 아이디의 1번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (3,'3번째 아이디의 2번글','{$date}');
+        INSERT INTO test2Table (Fk_id,Contents,Regist_day) VALUE (4,'4번째 아이디의 1번글','{$date}');
+    ";
+
+    return $query;
+}
+
+function createTable() {
+    $query[1] = "
+    CREATE TABLE testTable(
+      Idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      Id VARCHAR(10) NOT NULL UNIQUE KEY,
+      Regist_day DATETIME NOT NULL
+    )ENGINE=InnoDB DEFAULT CHAR SET = UTF8; 
+    ";
+
+    $query[2] = "
+    CREATE TABLE test2Table(
+      Idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      Fk_id INT NOT NULL,
+      Contents TEXT NOT NULL,
+      Regist_day DATETIME NOT NULL,
+      FOREIGN KEY (`Fk_id`) REFERENCES `testTable` (`Idx`)
+    )ENGINE=InnoDB DEFAULT CHAR SET = UTF8;
+    ";
+
+    return $query;
+}
 ?>
+
+<?php
+$query = 'SELECT * FROM testtable left JOIN test2Table on testtable.Idx = test2Table.Fk_id';
+$result = $mysqli->query($query);
+?>
+
+<table cellpadding="0" cellspacing="0" border="1">
+
+</table>
+
 </body>
 </html>
